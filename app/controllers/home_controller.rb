6c57395ac0e63176d10fab7bcf93c0309535ccc9
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2025-03-08 13:29:37 +0100

    feat: enhance dashboard UI with improved statistics display and user permissions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-    console
     @hello = "Hello, World!"
   end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  enum :week_day, { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 1 }
+  enum :week_day, { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6 }
 
   belongs_to :teacher, class_name: "Teacher", foreign_key: "teacher_id"
   belongs_to :school_class
